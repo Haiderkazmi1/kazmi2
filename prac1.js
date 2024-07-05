99f770c29222adbce029c8578643b7abd889a6e2
Haiderkazmi1 <haiderkazmi600@gmail.com>
2024-07-06 02:16:37 +0500

    Add prac1.js file

---
+++ prac1.js
@@ -0,0 +1,9 @@
+// console.log("Hi! This is Kazmi")
+const fruits = ["apple", "banana", "cherry","Pineapple","Pomegrenade"];
+for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i]==="Pineapple") {
+        console.log(fruits[i]+" My favourite");
+    } else {
+        console.log(fruits[i]);
+    }
+}
